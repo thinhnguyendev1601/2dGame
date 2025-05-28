67b0c28f1a41fe1f42b16749bee1433582be6eda
buiTD <tuandatbui85@gmail.com>
2025-05-28 22:14:25 +0700

    Update UtilityTool.java
    Signed-off-by: buiTD <tuandatbui85@gmail.com>

--- src/main/UtilityTool.java
+++ src/main/UtilityTool.java
@@ -1,18 +1,26 @@
 package main;
 
-import java.awt.*;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+/**
+ * Utility class for common operations like image scaling
+ */
 public class UtilityTool {
-
-    public BufferedImage scaleImage(BufferedImage original,int width, int height)
-    {
-        BufferedImage scaledImage = new BufferedImage(width,height,original.getType());
+    
+    /**
+     * Scales an image to the specified width and height
+     * @param original Original image to scale
+     * @param width Target width
+     * @param height Target height
+     * @return Scaled BufferedImage
+     */
+    public BufferedImage scaleImage(BufferedImage original, int width, int height) {
+        BufferedImage scaledImage = new BufferedImage(width, height, original.getType());
         Graphics2D g2 = scaledImage.createGraphics();
-        g2.drawImage(original,0,0,width,height,null);
+        g2.drawImage(original, 0, 0, width, height, null);
         g2.dispose();
-
+        
         return scaledImage;
     }
-
 }
