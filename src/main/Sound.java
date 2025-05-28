23f059fa981c3e3685c5b2b827ad4d10f5b732ff
buiTD <tuandatbui85@gmail.com>
2025-05-28 22:03:57 +0700

    Update Sound.java
    Signed-off-by: buiTD <tuandatbui85@gmail.com>

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -1,69 +1,81 @@
 package main;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.*;
+import java.io.IOException;
 import java.net.URL;
 
 public class Sound {
+    GamePanel gp;
     Clip clip;
     URL soundURL[] = new URL[30];
-    FloatControl fc;
-    int volumeScale = 3;
-    float volume;
-
-    public Sound()
-    {
-        soundURL[0] = getClass().getResource("/sound/background.wav");
-        soundURL[1] = getClass().getResource("/sound/paper.wav");
-        soundURL[2] = getClass().getResource("/sound/chalkOnTheWall.wav");
-        soundURL[3] = getClass().getResource("/sound/indoorWalk.wav");
-        soundURL[4] = getClass().getResource("/sound/outdoorWalk.wav");
-        soundURL[5] = getClass().getResource("/sound/radioCrafting.wav")
+    float volume = 0.5f; // Volume from 0.0 to 1.0
+    boolean isMuted = false;
+    
+    public Sound(GamePanel gp) {
+        this.gp = gp;
+        
+        // Load sound files
+        soundURL[0] = getClass().getResource("/sound/background.wav");      // Background music
+        soundURL[1] = getClass().getResource("/sound/paper.wav");           // Paper sound
+        soundURL[2] = getClass().getResource("/sound/chalkOnTheWall.wav");  // Chalk sound
+        soundURL[3] = getClass().getResource("/sound/indoorWalk.wav");      // Indoor footsteps
+        soundURL[4] = getClass().getResource("/sound/outdoorWalk.wav");     // Outdoor footsteps
+        soundURL[5] = getClass().getResource("/sound/radioCrafting.wav");   // Crafting sound
     }
-
-    public void setFile(int i)           // Java Sound File Opening Format
-    {
+    
+    public void setFile(int i) {
         try {
             AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
             clip = AudioSystem.getClip();
             clip.open(ais);
-            fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN); //pass value for clip // -80f to 6f // 6 is max. -80f = 0
-        }
-        catch (Exception e)
-        {
+            
+            // Set volume
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
+            gainControl.setValue(dB);
+            
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
-        checkVolume();
     }
-
-    public void play()
-    {
-        clip.start();
-    }
-
-    public void loop()
-    {
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
-    }
-
-    public void stop()
-    {
-        clip.stop();
-    }
-
-    public void checkVolume()
-    {
-        switch (volumeScale)
-        {
-            case 0 : volume = -80f; break;
-            case 1 : volume = -20f; break;
-            case 2 : volume = -12f; break;
-            case 3 : volume = -5f; break;
-            case 4 : volume = 1f; break;
-            case 5 : volume = 6f; break;
+    
+    public void play() {
+        if (!isMuted) {
+            clip.start();
         }
-        fc.setValue(volume);
+    }
+    
+    public void loop() {
+        if (!isMuted) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+    }
+    
+    public void stop() {
+        if (clip != null) {
+            clip.stop();
+        }
+    }
+    
+    public void setVolume(float volume) {
+        this.volume = Math.max(0.0f, Math.min(1.0f, volume));
+        if (clip != null) {
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            float dB = (float) (Math.log(this.volume) / Math.log(10.0) * 20.0);
+            gainControl.setValue(dB);
+        }
+    }
+    
+    public void toggleMute() {
+        isMuted = !isMuted;
+        if (isMuted) {
+            stop();
+        } else {
+            play();
+        }
+    }
+    
+    public boolean isMuted() {
+        return isMuted;
     }
 }
