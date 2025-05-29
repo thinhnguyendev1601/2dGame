450cc8cd24c821ba84b698b8c084782d0854dc58
thinhnguyendev1601 <thinhduc1601@gmail.com>
2025-05-29 21:44:33 +0700

    Update EventHandler.java
    Signed-off-by: thinhnguyendev1601 <thinhduc1601@gmail.com>

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -77,7 +77,7 @@ public class EventHandler{
             else if(hit(1,12,9, "up") == true) {speak(gp.npc[1][0]);} //merchant
 
             else if(hit(0,12,9, "any") == true) {teleport(2,9,41,gp.dungeon);} //to stairs from the living room
-            else if(hit(2,9,41, "any") == true) {teleport(0,12,9,gp.outside);} //to outside
+            else if(hit(2,9,41, "any") == true) {teleport(0,12,9,gp.outside);} //return back to the living room from the stairs 
             else if(hit(2,8,7, "any") == true) {teleport(3,32,41,gp.dungeon);} //to B2
             else if(hit(3,26,41, "any") == true) {teleport(2,8,7,gp.dungeon);} //to B1
             else if(hit(3,25,27, "any") == true) {skeletonLord();} //BOSS
